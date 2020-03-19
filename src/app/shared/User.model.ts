9a80ad2e6d2f1db7586a2b7408d540de12870a90
rushikesh-patil <rushi.patil11@gmail.com>
2020-03-19 14:44:23 +0530

    March commit

--- src/app/shared/User.model.ts
+++
@@ -1,8 +0,0 @@
-export class User{
-    public userName:String;
-    public password:String;
-    constructor(userName: String ,password: String){
-        this.userName=userName;
-        this.password=password;  
-    }
-}
